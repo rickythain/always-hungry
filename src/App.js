0160a161e2751609282326f4a85cca9d01fd5601
Anantharao1998 <anantharao1998@gmail.com>
2021-10-19 13:35:43 +0800

    feat: Able to GET data from API

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import logo from "./logo.svg";
 import "./App.css";
-import { BrowserRouter, Route, Switch, Link } from "react-router-dom";
-import React from "react";
+import React, { useState } from "react";
+import Axios from "axios";
 
 function App() {
+  const [mealName, setmealName] = useState("");
+
+  var url = `https://www.themealdb.com/api/json/v1/1/search.php?s=${mealName}`;
+
+  async function getRecipes() {
+    var result = await Axios.get(url);
+    console.log(result.data);
+  }
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+    getRecipes();
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form onSubmit={onSubmit}>
+        <input
+          type="text"
+          placeholder="Type meal name"
+          value={mealName}
+          onChange={(e) => setmealName(e.target.value)}
+        ></input>
+        <input type="submit" value="Search" />
+      </form>
     </div>
   );
 }
