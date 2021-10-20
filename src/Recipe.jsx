8ac0cbe08e9ec80d3a03c3ce7dcac17e3842d448
Ricky Thain <rickythain@gmail.com>
2021-10-20 15:34:19 +0800

    add meal suggestions on recipe page

--- src/Recipe.jsx
+++ src/Recipe.jsx
@@ -7,25 +7,51 @@ import {
   useLocation,
 } from "react-router-dom";
 import React from "react";
+import MealCard from "./MealCard";
 
 function Recipe() {
   // obtain meal data sent
   const location = useLocation();
-  const mealObject = location.state?.meal;
-  const mealData = mealObject.meal;
 
+  // states
   const [meal, setMeal] = React.useState({});
   const [mealTags, setMealTags] = React.useState("");
   const [mealIngredientList, setMealIngredientList] = React.useState([]);
   const [mealYouTubeLink, setMealYouTubeLink] = React.useState("");
+  const [similarMeals, setSimilarMeals] = React.useState([]);
 
-  // retrieve full details of the meal
-  let apiAddress = "https://www.themealdb.com/api/json/v1/1/search.php?s=";
-  let mealQuery = mealData.strMeal;
-  mealQuery = mealQuery.split(" ").join("%20");
-  let apiFullAddress = apiAddress + mealQuery;
+  const shuffle = (array) => {
+    let currentIndex = array.length,
+      randomIndex;
 
+    // While there remain elements to shuffle...
+    while (currentIndex != 0) {
+      // Pick a remaining element...
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex--;
+
+      // And swap it with the current element.
+      [array[currentIndex], array[randomIndex]] = [
+        array[randomIndex],
+        array[currentIndex],
+      ];
+    }
+
+    return array;
+  };
+
+  // update recipe when meal changes
   React.useEffect(() => {
+    // const location = useLocation();
+    const mealObject = location.state?.meal;
+    const mealData = mealObject.meal;
+
+    // retrieve full details of the meal
+    let apiAddress = "https://www.themealdb.com/api/json/v1/1/search.php?s=";
+    let mealQuery = mealData.strMeal;
+    mealQuery = mealQuery.split(" ").join("%20");
+    let apiFullAddress = apiAddress + mealQuery;
+
     fetch(apiFullAddress)
       .then((data) => {
         return data.json();
@@ -36,15 +62,18 @@ function Recipe() {
         // );
         setMeal(dataJson.meals[0]);
       });
-  }, []);
+  }, [location.state?.meal]);
 
+  // update variables when meal data is updated
   React.useEffect(() => {
+    // set meal tags into a variable
     if (meal.strTags) {
       setMealTags(meal.strTags.split(",").join(", "));
     } else {
       setMealTags("");
     }
 
+    // combine each ingredient with its corresponding measure
     setMealIngredientList([]);
     for (let i = 1; i < 21; i++) {
       let ingredientIndex = "strIngredient" + i;
@@ -59,10 +88,39 @@ function Recipe() {
       }
     }
 
+    // change youtube link from watch to embed
     if (meal.strYoutube) {
       setMealYouTubeLink(meal.strYoutube.replace("watch?v=", "embed/"));
     } else {
       setMealYouTubeLink("");
+    }
+
+    if (meal.strCategory) {
+      let categoryName = meal.strCategory;
+      let addressGetMealOnCategory =
+        "https://www.themealdb.com/api/json/v1/1/filter.php?c=";
+      let fullAddress = addressGetMealOnCategory + categoryName;
+
+      console.log(
+        `categoryname: ${categoryName}, apifulladdress: ${fullAddress}`
+      );
+
+      fetch(fullAddress)
+        .then((data) => {
+          return data.json();
+        })
+        .then((dataJson) => {
+          // setMeals(Array.from(dataJson.meals));
+          let receivedMeals = Array.from(dataJson.meals);
+          receivedMeals = receivedMeals.filter(
+            (item) => item.strMeal !== meal.strMeal
+          );
+          receivedMeals = shuffle(receivedMeals);
+          receivedMeals = receivedMeals.slice(0, 3);
+          setSimilarMeals([]);
+          setSimilarMeals(receivedMeals);
+          // console.log("meals: " + JSON.stringify(dataJson.meals, null, 2));
+        });
     }
   }, [meal]);
 
@@ -90,6 +148,12 @@ function Recipe() {
           </div>
           <p>Instructions</p>
           <p className="trial">{meal.strInstructions}</p>
+        </div>
+
+        <div>
+          {similarMeals.map((meal, index) => {
+            return <MealCard meal={meal} />;
+          })}
         </div>
       </div>
     </div>
