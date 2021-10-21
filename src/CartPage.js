7bac80369daafde57319e3db08d0159f907fac7f
Balaganabathy Krishnanmoorthy <gana.ganabathy@gmail.com>
2021-10-21 09:46:20 +0800

    add cart done without local storage

--- src/CartPage.js
+++ src/CartPage.js
@@ -8,7 +8,7 @@ import { Link } from "react-router-dom";
 const CartPage = () => {
   const [total, setTotal] = useState();
 
-  const { cart } = useContext(Cart);
+  const { cart, setCart } = useContext(Cart);
 
   //   useEffect(() => {
   //     setTotal(cart.reduce((acc, curr) => acc + Number(curr.price), 0));
@@ -18,14 +18,28 @@ const CartPage = () => {
     <div>
       <span style={{ fontSize: 30 }}>My Cart</span>
       <br />
-      <span style={{ fontSize: 30 }}>Total: {total}</span>
+      <span style={{ fontSize: 30 }}>
+        You have {cart.length} item in Favourites
+      </span>
       <div className="productContainer">
         {/* {cart && cart.map((prod) => <MealCard prod={prod} key={prod.idMeal} />)} */}
         {cart &&
           cart.map((meal) => (
-            <div className="card">
-              <img src={meal.strMealThumb} />
-              {meal.strMeal}
+            <div className="productContainer">
+              <Link to={{ pathname: "/recipe", state: { meal: { meal } } }}>
+                <div className="card">
+                  <img src={meal.strMealThumb} />
+                  {meal.strMeal}
+                </div>
+              </Link>
+              <button
+                className="remove"
+                onClick={() => {
+                  setCart(cart.filter((c) => c.idMeal !== meal.idMeal));
+                }}
+              >
+                Remove from Cart
+              </button>
             </div>
           ))}
       </div>
