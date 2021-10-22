4b5b81a20df0901ca2641d06119e25d745bdb85d
Ricky Thain <rickythain@gmail.com>
2021-10-22 08:45:37 +0800

    add fav count to nav-item fav

--- src/Component/Navbar.js
+++ src/Component/Navbar.js
@@ -4,6 +4,8 @@ import { Link } from "react-router-dom";
 import ReorderIcon from "@material-ui/icons/Reorder";
 import "../Styles/Navbar.css";
 import { Cart } from "../Context";
+import heart from "../Assets/heart02.png";
+import favedHeart from "../Assets/full_heart.png";
 
 function Navbar() {
   const [openLinks, setOpenLinks] = useState(false);
@@ -12,6 +14,7 @@ function Navbar() {
   const toggleNavbar = () => {
     setOpenLinks(!openLinks);
   };
+
   return (
     <div className="navbar">
       <div className="title" id={openLinks ? "open" : "close"}>
@@ -28,7 +31,20 @@ function Navbar() {
       </div>
       <div className="rightSide">
         <Link to="/"> Home </Link>
-        <Link to="/cart">Favourites</Link>
+        <Link to="/cart" className="nav-fav">
+          Favourites
+          {cart.length > 0 ? (
+            <div className="fav">
+              <img src={favedHeart} />
+              <div className="fav-count fav-text-white">{cart.length}</div>
+            </div>
+          ) : (
+            <div className="fav">
+              <img src={heart} />
+              <div className="fav-count fav-text-black">0</div>
+            </div>
+          )}
+        </Link>
         {/* <Link to="/recipe"> Recipe </Link> */}
         {/* <Link to="/about"> About </Link> */}
         <button onClick={toggleNavbar}>
